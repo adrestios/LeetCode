1321f71f6f992a5bcfe2a31fe8efa474521eaab5
adrestios <adrestios@gmail.com>
2021-03-21 22:21:52 +0800

    add: 682

---
+++ src/682. Baseball Game.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+var calPoints = function(ops) {
+  let res = [];
+  for(let i = 0; i < ops.length; i++) {
+      switch(ops[i]) {
+          case '+': 
+              res.push(res[res.length - 1] + res[res.length - 2]);
+              break;
+          case 'D':
+              res.push(res[res.length - 1] * 2);
+              break;
+          case 'C':
+              res.pop();
+              break;
+          default: 
+              res.push(Number(ops[i]));
+      }s.push(Number(ops[i]));
+      }
+  }
+  console.log(res)
+  return res.reduce((acc, cur) => {
+      return  acc + cur;
+  }, 0)
+};
